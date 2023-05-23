51467330c07acc177537393bd1a34aba8241a26b
Terry Moreland <tsmoreland@live.com>
2023-05-23 22:21:39 +0100

    JPA repository for Contact

---
+++ backend/src/test/java/com/tsmoreland/misc/crm/model/ContactRepository.java
@@ -0,0 +1,7 @@
+package com.tsmoreland.misc.crm.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContactRepository extends CrudRepository<Contact, Long> {
+
+}
