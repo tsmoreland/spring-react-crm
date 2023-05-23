dbe3f0cf3e9eb2382918a7f4f4fd406cfc93f82c
Terry Moreland <tsmoreland@live.com>
2023-05-23 22:21:21 +0100

    initial entity used in database

---
+++ backend/src/test/java/com/tsmoreland/misc/crm/model/Contact.java
@@ -0,0 +1,22 @@
+package com.tsmoreland.misc.crm.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Contact {
+    private @Id @GeneratedValue Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    protected Contact() {}
+    public Contact(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
